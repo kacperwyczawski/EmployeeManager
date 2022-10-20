e2a24d153a6dfcbbf8b448b15a7bc53e4cbe53d7
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-20 17:32:16 +0200

    Create migration for initializing db

---
+++ EmployeeManager/Migrations/20221020153139_InitialCreate.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManager.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "departments",
+                columns: table => new
+                {
+                    dept_no = table.Column<string>(type: "char(4)", fixedLength: true, maxLength: 4, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    dept_name = table.Column<string>(type: "varchar(40)", maxLength: 40, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.dept_no);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    emp_no = table.Column<int>(type: "int", nullable: false),
+                    birth_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    first_name = table.Column<string>(type: "varchar(14)", maxLength: 14, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    last_name = table.Column<string>(type: "varchar(16)", maxLength: 16, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    gender = table.Column<string>(type: "enum('M','F')", nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    hire_date = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.emp_no);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+
+            migrationBuilder.CreateTable(
+                name: "dept_emp",
+                columns: table => new
+                {
+                    emp_no = table.Column<int>(type: "int", nullable: false),
+                    dept_no = table.Column<string>(type: "char(4)", fixedLength: true, maxLength: 4, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    from_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    to_date = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => new { x.emp_no, x.dept_no })
+                        .Annotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+                    table.ForeignKey(
+                        name: "dept_emp_ibfk_1",
+                        column: x => x.emp_no,
+                        principalTable: "employees",
+                        principalColumn: "emp_no",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "dept_emp_ibfk_2",
+                        column: x => x.dept_no,
+                        principalTable: "departments",
+                        principalColumn: "dept_no",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+
+            migrationBuilder.CreateTable(
+                name: "dept_manager",
+                columns: table => new
+                {
+                    emp_no = table.Column<int>(type: "int", nullable: false),
+                    dept_no = table.Column<string>(type: "char(4)", fixedLength: true, maxLength: 4, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    from_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    to_date = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => new { x.emp_no, x.dept_no })
+                        .Annotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+                    table.ForeignKey(
+                        name: "dept_manager_ibfk_1",
+                        column: x => x.emp_no,
+                        principalTable: "employees",
+                        principalColumn: "emp_no",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "dept_manager_ibfk_2",
+                        column: x => x.dept_no,
+                        principalTable: "departments",
+                        principalColumn: "dept_no",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+
+            migrationBuilder.CreateTable(
+                name: "salaries",
+                columns: table => new
+                {
+                    emp_no = table.Column<int>(type: "int", nullable: false),
+                    from_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    salary = table.Column<int>(type: "int", nullable: false),
+                    to_date = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => new { x.emp_no, x.from_date })
+                        .Annotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+                    table.ForeignKey(
+                        name: "salaries_ibfk_1",
+                        column: x => x.emp_no,
+                        principalTable: "employees",
+                        principalColumn: "emp_no",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+
+            migrationBuilder.CreateTable(
+                name: "titles",
+                columns: table => new
+                {
+                    emp_no = table.Column<int>(type: "int", nullable: false),
+                    title = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    from_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    to_date = table.Column<DateOnly>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => new { x.emp_no, x.title, x.from_date })
+                        .Annotation("MySql:IndexPrefixLength", new[] { 0, 0, 0 });
+                    table.ForeignKey(
+                        name: "titles_ibfk_1",
+                        column: x => x.emp_no,
+                        principalTable: "employees",
+                        principalColumn: "emp_no",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "dept_name",
+                table: "departments",
+                column: "dept_name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "dept_no",
+                table: "dept_emp",
+                column: "dept_no");
+
+            migrationBuilder.CreateIndex(
+                name: "dept_no1",
+                table: "dept_manager",
+                column: "dept_no");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dept_emp");
+
+            migrationBuilder.DropTable(
+                name: "dept_manager");
+
+            migrationBuilder.DropTable(
+                name: "salaries");
+
+            migrationBuilder.DropTable(
+                name: "titles");
+
+            migrationBuilder.DropTable(
+                name: "departments");
+
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
