edd3691b09c7cd98239033341951cf147d603b96
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-21 09:34:37 +0200

    Create EmployeeService.cs

---
+++ EmployeeManager/Services/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManager.Data;
+using EmployeeManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManager.Services;
+
+public class EmployeeService
+{
+    private readonly EmployeeManagerContext _context;
+
+    public EmployeeService(EmployeeManagerContext context) =>
+        _context = context;
+
+    public async Task<List<Employee>> GetEmployeesAsync() =>
+        await _context.Employees.AsNoTracking().ToListAsync();
+}
