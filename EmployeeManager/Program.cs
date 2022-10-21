3eb1269cc1c1a677435bd1110f98bae6d9bf214d
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-21 09:05:43 +0200

    Delete even more unnecessary things

--- EmployeeManager/Program.cs
+++ EmployeeManager/Program.cs
@@ -1,17 +1,10 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using EmployeeManager.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
