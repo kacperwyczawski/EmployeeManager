7e068c78d830a2bb9d534a4822dee7ecbf4d1459
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-24 10:22:22 +0200

    Update DepartmentService.cs

--- EmployeeManager/Services/DepartmentService.cs
+++ EmployeeManager/Services/DepartmentService.cs
@@ -11,6 +11,33 @@ public class DepartmentService
     public DepartmentService(EmployeeManagerContext context) =>
         _context = context;
 
-    public async Task<List<Department>> GetDepartmentsAsync() =>
-        await _context.Departments.ToListAsync();
+    public string GetDepartmentName(int employeeId)
+    {
+        var recentlyEmployeeAt = _context.DeptEmps
+            .AsNoTracking()
+            .Include(de => de.DeptNoNavigation)
+            .Where(de => de.EmpNo == employeeId)
+            .OrderByDescending(de => de.ToDate)
+            .FirstOrDefault();
+
+        var recentlyManagerAt = _context.DeptManagers
+            .AsNoTracking()
+            .Include(de => de.DeptNoNavigation)
+            .Where(dm => dm.EmpNo == employeeId)
+            .OrderByDescending(dm => dm.ToDate)
+            .FirstOrDefault();
+
+        if (recentlyEmployeeAt is null && recentlyManagerAt is not null)
+            return recentlyManagerAt.DeptNoNavigation.DeptName;
+
+        if (recentlyEmployeeAt is not null && recentlyManagerAt is null)
+            return recentlyEmployeeAt.DeptNoNavigation.DeptName;
+        
+        if (recentlyEmployeeAt is not null && recentlyManagerAt is not null)
+            return recentlyEmployeeAt.ToDate > recentlyManagerAt.ToDate
+                ? recentlyEmployeeAt.DeptNoNavigation.DeptName
+                : recentlyManagerAt.DeptNoNavigation.DeptName;
+        
+        throw new InvalidOperationException("Employee is not assigned to any department.");
+    }
 }
