3dace97cb11d026db0754f8a6abd13b0d7690506
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-24 10:22:43 +0200

    Create TitleService.cs

---
+++ EmployeeManager/Services/TitleService.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManager.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManager.Services;
+
+public class TitleService
+{
+    private readonly EmployeeManagerContext _context;
+
+    public TitleService(EmployeeManagerContext context) =>
+        _context = context;
+    
+    public string GetTitleName(int employeeId)
+    {
+        var titles = _context.Titles
+            .AsNoTracking()
+            .Where(t => t.EmpNo == employeeId);
+
+        return titles.SingleOrDefault(t => t.ToDate == null)?.Title1
+            ?? titles.OrderByDescending(t => t.ToDate).First().Title1;
+    }
+}
