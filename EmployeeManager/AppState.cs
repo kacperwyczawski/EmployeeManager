fe3d582a072ca63a111eb0f600db81fb6379d154
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-25 11:29:33 +0200

    Add department filter

--- EmployeeManager/AppState.cs
+++ EmployeeManager/AppState.cs
@@ -2,14 +2,22 @@
 
 public class AppState
 {
-    public int ItemsPerPage { get; set; } = 20;
+    public AppState() => ResetToDefaults();
+
+    public int ItemsPerPage { get; set; }
     
     public GenderFilter Gender { get; set; }
+    
+    public (int from, int to)? SalaryRange { get; set; }
+    
+    public string? DepartmentName { get; set; }
     
     public void ResetToDefaults()
     {
         ItemsPerPage = 20;
         Gender = GenderFilter.Any;
+        SalaryRange = null;
+        DepartmentName = null;
     }
 
     public enum GenderFilter
