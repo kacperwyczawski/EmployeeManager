85fb6a582abc5e05f7675113ba888f236fa6e003
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-25 10:02:40 +0200

    Add gender filter

--- EmployeeManager/AppState.cs
+++ EmployeeManager/AppState.cs
@@ -4,8 +4,16 @@ public class AppState
 {
     public int ItemsPerPage { get; set; } = 20;
     
+    public GenderFilter Gender { get; set; }
+    
     public void ResetToDefaults()
     {
         ItemsPerPage = 20;
+        Gender = GenderFilter.Any;
+    }
+
+    public enum GenderFilter
+    {
+        Any, Male, Female
     }
 }
