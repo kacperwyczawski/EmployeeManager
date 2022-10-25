510cb1ba61c6dd28347ba1da09db66e4e0925c14
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-25 12:08:55 +0200

    Update UI and refactor filtering

--- EmployeeManager/AppState.cs
+++ EmployeeManager/AppState.cs
@@ -2,26 +2,49 @@
 
 public class AppState
 {
-    public AppState() => ResetToDefaults();
+    public AppState()
+    {
+        ResetToDefaults();
+    }
 
     public int ItemsPerPage { get; set; }
+
+    public Filter<char> GenderFilter { get; set; } = new('M');
+
+    public Filter<string> DepartmentFilter { get; set; } = new("Customer Service");
     
-    public GenderFilter Gender { get; set; }
-    
-    public (int from, int to)? SalaryRange { get; set; }
-    
-    public string? DepartmentName { get; set; }
+    public Filter<(int from, int to)> SalaryFilter { get; set; } = new ((12000, 18000));
     
     public void ResetToDefaults()
     {
         ItemsPerPage = 20;
-        Gender = GenderFilter.Any;
-        SalaryRange = null;
-        DepartmentName = null;
+        GenderFilter.IsActive = false;
+        DepartmentFilter.IsActive = false;
+        SalaryFilter.IsActive = false;
     }
 
-    public enum GenderFilter
+    public class Filter<T>
     {
-        Any, Male, Female
+        public Filter(T allowedValue, bool isActive = false)
+        {
+            AllowedValue = allowedValue;
+            IsActive = isActive;
+        }
+
+        public T AllowedValue { get; set; }
+        
+        public bool IsActive;
+        
+        public bool GetAllowedValue(out T value)
+        {
+            if (IsActive)
+            {
+                value = AllowedValue!;
+                return true;
+            }
+
+            value = default!;
+            return false;
+        }
     }
 }
